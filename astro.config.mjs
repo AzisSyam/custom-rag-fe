eb1f92757895fb90f3c0b61901009882dd97d8ef
AzisSyam <abdazissyam99@gmail.com>
2026-05-07 09:28:22 +0800

    fix: update adapter to vercel for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import tailwindv4 from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
   }),
   integrations: [react()],
   vite: {
